f867bc5b96592e99b5f3ad4b91c1926d830e7dfa
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-10-26 13:49:56 +0000

    Get completions from .NET

--- MonacoRazor/StaticAssets/main.ts
+++ MonacoRazor/StaticAssets/main.ts
@@ -1,16 +1,46 @@
 import * as monaco from 'monaco-editor';
 
 type DotNetObjectReference = any;
+const registeredCompletionHandlers = {};
 
 export function init(element: HTMLElement, component: DotNetObjectReference, language: string, value: string) {
+    if (!registeredCompletionHandlers[language]) {
+        registeredCompletionHandlers[language] = true;
+        monaco.languages.registerCompletionItemProvider(language, new RemoteCompletionItemProvider());
+    }
+    
     const editor = monaco.editor.create(element, {
         value: value,
         language: language,
         minimap: { enabled: false },
     });
 
+    editor.getModel()['blazorComponent'] = component;
+
     editor.onDidBlurEditorText(() => {
         const value = editor.getValue();
         component.invokeMethodAsync('HandleEditorBlur', value);
     });
+}
+
+class RemoteCompletionItemProvider implements monaco.languages.CompletionItemProvider {
+    public triggerCharacters = ['.'];
+
+    async provideCompletionItems(model: monaco.editor.ITextModel, position: monaco.Position, context: monaco.languages.CompletionContext, token: monaco.CancellationToken): Promise<monaco.languages.CompletionList> {
+        const component = model['blazorComponent'] as DotNetObjectReference;
+        if (component) {
+            const value = model.getValue();
+            const suggestions = await component.invokeMethodAsync('GetCompletions', value, position);
+            return {
+                suggestions: suggestions.map(s => ({
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    label: s.insertText,
+                    insertText: s.insertText,
+                    range: null,
+                }))
+            };
+        } else {
+            return { suggestions: [] };
+        }
+    }
 }
