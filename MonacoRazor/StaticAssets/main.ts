f403bef6174e1ff84ba4709f87c3bbc02d974646
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-10-26 13:18:13 +0000

    Basic binding

--- MonacoRazor/StaticAssets/main.ts
+++ MonacoRazor/StaticAssets/main.ts
@@ -1,9 +1,16 @@
 import * as monaco from 'monaco-editor';
 
-export function init(element: HTMLElement, language: string, value: string) {
-    monaco.editor.create(element, {
+type DotNetObjectReference = any;
+
+export function init(element: HTMLElement, component: DotNetObjectReference, language: string, value: string) {
+    const editor = monaco.editor.create(element, {
         value: value,
         language: language,
         minimap: { enabled: false },
+    });
+
+    editor.onDidBlurEditorText(() => {
+        const value = editor.getValue();
+        component.invokeMethodAsync('HandleEditorBlur', value);
     });
 }
